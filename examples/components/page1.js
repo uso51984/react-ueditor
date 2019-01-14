dddc6d98b29b039a73f6f97f57731f3e819bdc6e
Edmund chend <Edmund.Chen@activenetwork.com>
2019-01-14 16:25:44 +0800

    fix: value受控时，每次输入，光标都会重置在最左边，导致输入的顺序颠倒。

--- examples/components/page1.js
+++ examples/components/page1.js
@@ -4,16 +4,19 @@ import Ueditor from '../../src'
 const value = '<p><a style="font-family: &quot;PingFang SC&quot;; font-size: medium; white-space: normal;">受控设置value值</a></p>';
 export default class Page1 extends React.Component {
   state ={
-    data: ''
+    initValue: '',
+    nowValue: ''
   }
 
   onChange = (value) => {
     this.ueditorValue = value;
     console.log('onChange', this.ueditorValue)
+    this.setState({ nowValue: value });
   }
 
   onBlur = (value) => {
     this.ueditorValue = value;
+    this.setState({ nowValue: value });
     console.log('onBlur', this.ueditorValue)
   }
 
@@ -23,14 +26,14 @@ export default class Page1 extends React.Component {
         <button
           onClick={(e) => {
             e.preventDefault();
-            this.setState({ data: value })
+            this.setState({ initValue: '设置value值' })
           }}
         >
         设置value值
         </button>
 
-        <Ueditor onChange={this.onChange} value={this.state.data} />
-        <p>{this.state.data}</p>
+        <Ueditor onChange={this.onChange} onBlur={this.onBlur} value={this.state.initValue} />
+        <p>{this.state.nowValue}</p>
       </div>
     )
   }
